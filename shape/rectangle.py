d8fee7b26d8368773516d93f6355e6d3cea0c293
Navraj Singh <navrajsingh@rrc.ca>
2025-02-07 06:07:21 -0600

    Documentations added to the rectangle class

--- shape/rectangle.py
+++ shape/rectangle.py
@@ -12,7 +12,10 @@ class rectangle(Shape):
     """
     def __init__(self, color: str, length:int, width:int):
         """
-        
+        Attributes:
+            color: (str) color of the shape.
+            length: (int) the length of the rectangle in centimeters.
+            width: (int) the width of the rectangle in centimeters.
         """
         super().__init__(color)
         if not isinstance(length, int):
@@ -25,7 +28,7 @@ class rectangle(Shape):
 
     def __str__(self) -> str:
         """
-        
+        this method will return the string in the formatted way.
         """
         value = super().__str__()
         value+= f"\n This rectangle has four sides with the lengths of {{length}}, {{width}}, {{length}} and {{width}} centimeters."
@@ -33,13 +36,13 @@ class rectangle(Shape):
     
     def _calculate_area(self) -> float:
         """
-        
+        this method is to calcualte the area of the rectangle.
         """
         return self._length * self._width
     
     def calculate_perimeter(self) -> float:
         """
-        
+        this method is to calculate the perimeter of the rectangle.
         """
         return 2 * self._length + 2 * self._width
     
