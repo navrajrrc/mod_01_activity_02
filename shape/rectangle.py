c9e08e84c62c2a7b7e774762d6b7b2d088d7c9dc
Navraj Singh <navrajsingh@rrc.ca>
2025-02-07 06:04:13 -0600

    Initiated the rectangle class and its methods

--- shape/rectangle.py
+++ shape/rectangle.py
@@ -1,4 +1,46 @@
 """This module defines the Rectangle class."""
 
-__author__ = ""
+__author__ = "Navraj singh"
 __version__ = ""
+
+from shape.shape import Shape
+import math
+
+class rectangle(Shape):
+    """
+    This is a class for the shape rectangle.
+    """
+    def __init__(self, color: str, length:int, width:int):
+        """
+        
+        """
+        super().__init__(color)
+        if not isinstance(length, int):
+            raise ValueError("Length must be numeric.")
+        self._length = length
+
+        if not isinstance(width,int):
+            raise ValueError("Width must be numeric")
+        self._width = width
+
+    def __str__(self) -> str:
+        """
+        
+        """
+        value = super().__str__()
+        value+= f"\n This rectangle has four sides with the lengths of {{length}}, {{width}}, {{length}} and {{width}} centimeters."
+        return value
+    
+    def _calculate_area(self) -> float:
+        """
+        
+        """
+        return self._length * self._width
+    
+    def calculate_perimeter(self) -> float:
+        """
+        
+        """
+        return 2 * self._length + 2 * self._width
+    
+
