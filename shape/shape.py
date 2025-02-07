5bc9f53aa3b62def6300fdee1f5f615a89fd852a
Navraj Singh <navrajsingh@rrc.ca>
2025-02-07 03:22:41 -0600

    Added documentations for the class and methods

--- shape/shape.py
+++ shape/shape.py
@@ -12,19 +12,31 @@ class Shape(ABC):
     def __init__(self, color: str):
         """
         Attributes:
-            
+            color: (str) This is the color of the shape.
+
+        ValueError:
+            If the color is blank.
         """
         if len(color.strip()) == 0:
             raise ValueError("Color cannot be blank")
         self._color = color.strip()
 
     def __str__(self) -> str:
+        """
+        This method will return the color of the shape in a formatted way
+        """
         return f"The shape color is {self._color}."
     
     @abstractmethod
     def calcualte_area(self) -> float:
+        """
+        This is a method to calculate the area of the shape.
+        """
         pass
 
     @abstractmethod
     def calculate_perimeter(Self) -> float:
+        """
+        This is a method to calculate the perimeter of the shape.
+        """
         pass
